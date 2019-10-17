605e4886f07f2a4aff3b280eb0ca8053798f3ab5
Олег <olegsolovev506@gmail.com>
2019-10-17 21:42:45 +0400

    commit

---
+++ bot.js
@@ -0,0 +1,17 @@
+var TelegramBot = require("node-telegram-bot-api");
+var token = "711800064:AAEmRD8V49AOHFItrVaSYFYUJ2Qth48cJTQ";
+var bot = new TelegramBot(token, { polling: true });
+
+var rollDice = require("./services/rollService");
+
+bot.onText(/d(.+) (.+)/, function(msg, match) {
+  var fromId = msg.from.id;
+  var edges = match[1];
+  var times = match[2];
+  bot.sendMessage(
+    fromId,
+    `Результат: ${rollDice(edges, times)
+      .map(i => i + " ")
+      .join("")}`
+  );
+});
